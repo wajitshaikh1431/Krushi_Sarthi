a03e0995e44ed7077d51227f9c4bb4a059e4a8be
codersalman <coder88salman@gmail.com>
2022-11-22 00:31:45 +0530

    auth with firebase added in the signup and login

--- app/src/main/java/www/wajit/krushisarthi/home_activity.java
+++ app/src/main/java/www/wajit/krushisarthi/home_activity.java
@@ -1,18 +1,43 @@
 package www.wajit.krushisarthi;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class home_activity extends AppCompatActivity {
 
+    private TextView  UserNameText;
+    private Button Btn;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        UserNameText = findViewById(R.id.username);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String username = user.getDisplayName();
+
+        if (username == null || username == "")
+        {
+            UserNameText.setText("Hello User");
+        }else{
+
+            UserNameText.setText(username);
+        }
+
     }
+
 
     public void btnprof(View view) {
         Intent i = new Intent(this, activity_profile.class);
--- app/src/main/java/www/wajit/krushisarthi/login_activity.java
+++ app/src/main/java/www/wajit/krushisarthi/login_activity.java
@@ -121,7 +121,6 @@ public class login_activity extends AppCompatActivity {
                                 }
                             }
                         });
-
     }
 
 
