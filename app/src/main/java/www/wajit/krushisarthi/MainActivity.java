593826cf318518b8a836ce9fd9b66ee96a25e6bf
codersalman <coder88salman@gmail.com>
2022-11-21 23:56:56 +0530

    auth with firebase added in the signup and login

--- app/src/main/java/www/wajit/krushisarthi/MainActivity.java
+++ app/src/main/java/www/wajit/krushisarthi/MainActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    String currentUser = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-
-
 
     }
 
 
-        public void btnnext(View view) {
-        Intent i = new Intent(MainActivity.this, signin_activity.class);
+    public void btnnext(View view) {
+        Intent i = new Intent(MainActivity.this, signup_activity.class);
         startActivity(i);
 
 
@@ -31,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(b);
 
     }
+
+    private void reload() { }
 }
 
 
---
+++ app/src/main/java/www/wajit/krushisarthi/signup_activity.java
@@ -0,0 +1,111 @@
+package www.wajit.krushisarthi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signup_activity extends AppCompatActivity {
+
+
+    private EditText emailTextView, passwordTextView, usernameTextView;
+    private Button Btn;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+        emailTextView = findViewById(R.id.email_u);
+        passwordTextView = findViewById(R.id.passwod_u);
+        Btn = findViewById(R.id.signup);
+usernameTextView = findViewById(R.id.username);
+        // Set on Click Listener on Registration button
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser()
+    {
+
+        // show the visibility of progress bar to show loading
+
+        // Take the value of two edit texts in Strings
+        String email, password,username;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+        username = usernameTextView.getText().toString();
+
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                                  Intent intent
+                                    = new Intent(signup_activity.this,
+                                    MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                            getApplicationContext(),
+                                            "Registration failed!!"
+                                                    + " Please try again later",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+
+                        }
+                    }
+                });
+    }
+}
